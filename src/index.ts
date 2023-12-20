3f0109a4ac9217cfce51495d2260971074997cba
uchel09 <ucelrussel77@gmail.com>
2023-12-19 19:37:20 -0800

    initialize project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const app: Application = express();
+const PORT: Number = 4000;
+
+app.use("/health", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: "200" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running in port ${PORT}`);
+});
